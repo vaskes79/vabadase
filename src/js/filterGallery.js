5586cd638f69747653dff0189e1ad9b5d22d6d8e
Vasily Guzov <vasily.guzov@gmail.com>
2019-03-14 20:18:38 +0300

    fix error #7

--- src/js/filterGallery.js
+++ src/js/filterGallery.js
@@ -6,31 +6,31 @@
  * Source:
  */
 
-function FilterGallery (options) {
-    var filter = this._filter = document.querySelector(options.selector);
-    var imgCont = this._imgCont = document.querySelector(options.imgCont);
-    var aImg = this._aImg = imgCont.querySelectorAll('a');
+// function FilterGallery (options) {
+//     var filter = this._filter = document.querySelector(options.selector);
+//     var imgCont = this._imgCont = document.querySelector(options.imgCont);
+//     var aImg = this._aImg = imgCont.querySelectorAll('a');
 
-    filter.addEventListener('click', this.filterImg.bind(this), false);
-}
+//     filter.addEventListener('click', this.filterImg.bind(this), false);
+// }
 
-FilterGallery.prototype.filterImg = function (event) {
-    var target = event.target;
-    var valueFilter = target.getAttribute('value');
-    for (var i=0; i<this._aImg.length; i++) {
-        var filter = this._aImg[i].getAttribute('data-filter');
-        if (filter.indexOf(valueFilter) == -1) {
-            this._aImg[i].classList.add('filterHide');
-            this._aImg[i].setAttribute('rel', '');
-        } else if (filter.indexOf(valueFilter) >= 0) {
-            this._aImg[i].classList.remove('filterHide');
-            this._aImg[i].setAttribute('rel', 'iLoad');
-        }
-    }
-};
+// FilterGallery.prototype.filterImg = function (event) {
+//     var target = event.target;
+//     var valueFilter = target.getAttribute('value');
+//     for (var i=0; i<this._aImg.length; i++) {
+//         var filter = this._aImg[i].getAttribute('data-filter');
+//         if (filter.indexOf(valueFilter) == -1) {
+//             this._aImg[i].classList.add('filterHide');
+//             this._aImg[i].setAttribute('rel', '');
+//         } else if (filter.indexOf(valueFilter) >= 0) {
+//             this._aImg[i].classList.remove('filterHide');
+//             this._aImg[i].setAttribute('rel', 'iLoad');
+//         }
+//     }
+// };
 
 
-var filter = new FilterGallery({
-    selector: '#filterGallery',
-    imgCont: '.imgCont'
-});
+// var filter = new FilterGallery({
+//     selector: '#filterGallery',
+//     imgCont: '.imgCont'
+// });
