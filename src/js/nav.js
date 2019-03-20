d84fc2d3f27690bb1abd83f2c7d546000a28be57
Vasily Guzov <vasily.guzov@gmail.com>
2019-03-20 10:04:31 +0300

    WIP #28 add resize event handler

--- src/js/nav.js
+++ src/js/nav.js
@@ -6,15 +6,31 @@ class Nav {
     this.activeClass = activeClass;
 
     this.clickHandler = this.clickHandler.bind(this);
+    this.resizeHandler = this.resizeHandler.bind(this);
   }
 
   init() {
-    this.elem.classList.add(this.activeClass);
+    if (window.innerWidth < 900) {
+      this.elem.classList.add(this.activeClass);
+    }
     this.btn.addEventListener('click', this.clickHandler);
+
+    this.resizeHandler();
 
     this.links.forEach(link =>
       link.addEventListener('click', this.clickHandler),
     );
+  }
+
+  resizeHandler() {
+    window.addEventListener('resize', e => {
+      let width = e.currentTarget.innerWidth;
+      if (width >= 900) {
+        this.elem.classList.remove(this.activeClass);
+      } else if (width <= 900) {
+        this.elem.classList.add(this.activeClass);
+      }
+    });
   }
 
   clickHandler() {
