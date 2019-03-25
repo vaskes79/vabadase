18e481ed6147f86e72e6d1253aee71689e81f5cd
Vasily Guzov <vasily.guzov@gmail.com>
2019-03-26 00:28:26 +0300

    WIP #24 create logic for open,close gallery

--- src/js/Gallery.js
+++ src/js/Gallery.js
@@ -1,39 +1,74 @@
 class Gallery {
   constructor(
-    elem = '.gallery-items',
-    galleryItems = '.gallery-items__item',
-    activeModifire = 'gallery-items--active',
-    classNameLink = '.gallery-items__link'
+    rootElem = '.gallery-items',
+    items = '.gallery-items__item',
+    itemsContainer = '.gallery-items__container',
+    openModifire = 'gallery-items--open',
+    links = '.gallery-items__link',
+    nextBtn = '.gallery-items__btn-R',
+    previousBtn = '.gallery-items__btn-L',
+    closeBtn = '.gallery-items__btn-close',
+    imgShow = '.gallery-items__img-show'
   ) {
-    this.elem = document.querySelector(elem);
-    this.galleryItems = document.querySelectorAll(galleryItems);
-    this.linkName = classNameLink;
-    this.activeModifire = activeModifire;
+    this.rootElem = document.querySelector(rootElem);
+    this.items = document.querySelectorAll(items);
+    this.closeBtn = document.querySelector(closeBtn);
+    this.nextBtn = document.querySelector(nextBtn);
+    this.previousBtn = document.querySelector(previousBtn);
+    this.itemsContainer = document.querySelector(itemsContainer);
+    this.imgShow = document.querySelector(imgShow);
+    this.linkName = links;
+    this.openModifire = openModifire;
 
     this.init = this.init.bind(this);
-    this.clickHandler = this.clickHandler.bind(this);
+    this.openHandler = this.openHandler.bind(this);
+    this.setHandlers = this.setHandlers.bind(this);
+    this.closeHandler = this.closeHandler.bind(this);
+    this.nextHandler = this.nextHandler.bind(this);
+    this.previousHandler = this.previousHandler.bind(this);
   }
 
   init() {
-    console.log(this);
     this.setHandlers();
   }
 
-  clickHandler(e) {
+  nextHandler() {
+    console.log('next');
+  }
+
+  previousHandler() {
+    console.log('previous');
+  }
+
+  openHandler(e) {
     e.preventDefault();
 
-    this.galleryItems.forEach(item =>
-      item.classList.remove(this.activeModifire)
-    );
+    switch (e.target.tagName) {
+      case 'A':
+        this.imgShow.src = e.target.href;
+        break;
+      case 'IMG':
+        this.imgShow.src = e.target.parentNode.href;
+        break;
+    }
 
-    e.target.parentNode.parentNode.classList.add(this.activeModifire);
+    this.rootElem.classList.add(this.openModifire);
+  }
+
+  closeHandler() {
+    this.rootElem.classList.remove(this.openModifire);
   }
 
   setHandlers() {
-    this.galleryItems.forEach(item => {
-      item
-        .querySelector(this.linkName)
-        .addEventListener('click', this.clickHandler);
+    this.itemsContainer.addEventListener('click', this.openHandler);
+    this.closeBtn.addEventListener('click', this.closeHandler);
+    this.nextBtn.addEventListener('click', this.nextHandler);
+    this.previousBtn.addEventListener('click', this.previousHandler);
+
+    document.addEventListener('keydown', e => {
+      if (e.keyCode === 27 || e.which === 27) {
+        this.closeHandler();
+      }
     });
   }
 }
