e00d18faa061a83486eb9ac4f72895057cc721bc
Vasily Guzov <vasily.guzov@gmail.com>
2019-03-27 17:15:43 +0300

    WIP add resize event listener #24

--- src/js/Gallery.js
+++ src/js/Gallery.js
@@ -15,6 +15,7 @@ class Gallery {
     };
 
     this.dataItems = [];
+    this.widthGallery = 900;
 
     this.openHandler = this.openHandler.bind(this);
     this.createImgItem = this.createImgItem.bind(this);
@@ -27,7 +28,6 @@ class Gallery {
   }
   createItems() {
     let {items, createImgItem, viewBox, dataItems, wrapItemsClassName} = this;
-    // 1. create array
     dataItems = [].map.call(items, item => {
       const {
         href: src,
@@ -40,34 +40,29 @@ class Gallery {
         description,
       };
     });
-    // 2. create elemen wrapItems
     let wrapItems = document.createElement('div');
     wrapItems.className = wrapItemsClassName;
-    // 3. create elements imgItems and add to this.wrapItems
     dataItems.forEach(item => {
       wrapItems.appendChild(createImgItem(item));
     });
-    // 4. get container items link and add this.wrapItems
     viewBox.appendChild(wrapItems);
-    // update this
+
     this.wrapItems = document.querySelector('.' + wrapItemsClassName);
     this.dataItems = dataItems;
-    // 5. set
     this.endPos = dataItems.length - 1;
-    // 6. set
     this.startPos = 0;
-    console.log(this);
   }
 
   createImgItem({src, allt, index, description}) {
     const {imgShowClassName} = this;
-
     const imgItem = document.createElement('img');
+
     imgItem.src = src;
-    imgItem.setAttribute('className', imgShowClassName);
+    imgItem.setAttribute('class', imgShowClassName);
     imgItem.setAttribute('allt', allt);
     imgItem.dataset['index'] = index;
     imgItem.dataset['description'] = description;
+
     return imgItem;
   }
 
@@ -82,21 +77,36 @@ class Gallery {
       btnPrevious,
       btnNext,
       container,
+      showGallery,
+      widthGallery,
     } = this;
 
-    // 1. set handlers for open gallery events
-    container.addEventListener('click', openHandler);
-    // 2. set handlers for close events
-    btnClose.addEventListener('click', closeHandler);
-    // 3. set handlers for controls events
-    btnPrevious.addEventListener('click', previousHandler);
-    btnNext.addEventListener('click', nextHandler);
-    // 4. set handlers for keyboard events
-    window.addEventListener('keydown', keyboardHandlers);
+    if (window.innerWidth >= this.widthGallery) {
+      container.addEventListener('click', openHandler);
+      btnClose.addEventListener('click', closeHandler);
+      btnPrevious.addEventListener('click', previousHandler);
+      btnNext.addEventListener('click', nextHandler);
+      window.addEventListener('keydown', keyboardHandlers);
+    }
+
+    window.addEventListener('resize', ({target: {innerWidth}}) => {
+      if (innerWidth >= widthGallery) {
+        container.addEventListener('click', openHandler);
+        btnClose.addEventListener('click', closeHandler);
+        btnPrevious.addEventListener('click', previousHandler);
+        btnNext.addEventListener('click', nextHandler);
+        window.addEventListener('keydown', keyboardHandlers);
+      } else {
+        container.removeEventListener('click', openHandler);
+        btnClose.removeEventListener('click', closeHandler);
+        btnPrevious.removeEventListener('click', previousHandler);
+        btnNext.removeEventListener('click', nextHandler);
+        window.removeEventListener('keydown', keyboardHandlers);
+      }
+    });
   }
 
   openHandler(e) {
-    // console.log('openHandler is working');
     e.preventDefault();
     let {
       openHandler,
@@ -105,9 +115,7 @@ class Gallery {
       gallery,
     } = this;
 
-    // 0. block scroll
     document.body.style.cssText = 'overflow: hidden';
-    // 1. get index
     let currentItem;
     switch (e.target.tagName) {
       case 'A':
@@ -117,11 +125,8 @@ class Gallery {
         currentItem = e.target.parentNode;
         break;
     }
-    // 2. set this.currentIndex
     this.currentIndex = parseInt(currentItem['dataset']['index']);
-    // 3. set position wrapItems
     setCurrentPosition();
-    // 4. show gallery
     gallery.classList.add(open);
   }
 
@@ -132,18 +137,15 @@ class Gallery {
     } = this;
 
     gallery.classList.remove(open);
-    // 0. block scroll
     document.body.style.cssText = 'overflow: auto';
   }
 
   nextHandler(e) {
-    console.log('nextHandler is working');
     this.currentIndex++;
     this.setCurrentPosition();
   }
 
   previousHandler(e) {
-    console.log('previousHandler is working');
     this.currentIndex--;
     this.setCurrentPosition();
   }
@@ -162,7 +164,6 @@ class Gallery {
     let prevKeys = [72, 37];
     // h, ←
     let nextKeys = [76, 39];
-
     let isClose = closeKeys.includes(keyCode) || closeKeys.includes(which);
     let isPrev = prevKeys.includes(keyCode) || prevKeys.includes(which);
     let isNext = nextKeys.includes(keyCode) || nextKeys.includes(which);
@@ -170,12 +171,10 @@ class Gallery {
     if (isClose && galleryIsOpen) {
       gallery.classList.remove(open);
     }
-
     if (isPrev && galleryIsOpen) {
       this.currentIndex--;
       setCurrentPosition();
     }
-
     if (isNext && galleryIsOpen) {
       this.currentIndex++;
       setCurrentPosition();
@@ -184,22 +183,18 @@ class Gallery {
 
   setCurrentPosition() {
     let {showControlls, currentIndex, startPos, endPos} = this;
-    // 1. calculate
     let offset = currentIndex * -100;
-    // 4. condition
+
     if (currentIndex <= startPos) offset = 0;
     if (currentIndex >= endPos) offset = endPos * -100;
-    // 5. update value
     this.wrapItems.style.cssText = `transform: translateX(${offset}%);`;
     showControlls();
   }
 
   showControlls() {
     let {currentIndex, endPos, btnPrevious, btnNext} = this;
-
     btnPrevious.style.cssText = 'cursor: pointer; opacity: 1; visible: visible';
     btnNext.style.cssText = 'cursor: pointer; opacity: 1; visible: visible';
-
     if (currentIndex <= 0) {
       btnPrevious.style.cssText = 'opacity: 0; visible: hidden';
       this.currentIndex = 0;
