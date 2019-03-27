f10daf830566f6f6d9fa257003504b27045cde0b
Vasily Guzov <vasily.guzov@gmail.com>
2019-03-27 12:02:04 +0300

    WIP #24 create openHandler closeHandler

--- src/js/Gallery.js
+++ src/js/Gallery.js
@@ -1,12 +1,22 @@
 class Gallery {
-  constructor(
-    gItemsLinks = '.gallery-items__link',
-    gViewBox = '.gallery-items__img-view'
-  ) {
-    this.items = document.querySelectorAll(gItemsLinks);
-    this.viewBox = document.querySelector(gViewBox);
-    this.wrapItemsClassName = 'gallery-items__wrap-items';
+  constructor(gal = '.gallery-items') {
+    this.gallery = document.querySelector(gal);
+    this.items = document.querySelectorAll(`${gal}__link`);
+    this.viewBox = document.querySelector(`${gal}__img-view`);
+    this.btnClose = document.querySelector(`${gal}__btn-close`);
+    this.container = document.querySelector(`${gal}__container`);
+
+    this.wrapItemsClassName = `${gal.slice(1)}__wrap-items`;
+    this.imgShowClassName = `${gal.slice(1)}__img-show`;
+    this.modifires = {
+      open: `${gal}--open`.slice(1),
+    };
+
     this.dataItems = [];
+
+    this.openHandler = this.openHandler.bind(this);
+    this.createImgItem = this.createImgItem.bind(this);
+    this.closeHandler = this.closeHandler.bind(this);
   }
   createItems() {
     let {items, createImgItem, viewBox, dataItems, wrapItemsClassName} = this;
@@ -43,17 +53,96 @@ class Gallery {
   }
 
   createImgItem({src, allt, index, description}) {
+    const {imgShowClassName} = this;
+
     const imgItem = document.createElement('img');
     imgItem.src = src;
-    imgItem.setAttribute('className', 'galley-items__img-show');
+    imgItem.setAttribute('className', imgShowClassName);
     imgItem.setAttribute('allt', allt);
     imgItem.dataset['index'] = index;
     imgItem.dataset['description'] = description;
     return imgItem;
   }
 
+  setHandlers() {
+    let {
+      openHandler,
+      closeHandler,
+      nextHandler,
+      previousHandler,
+      keyboardHandlers,
+      btnClose,
+      container,
+    } = this;
+
+    // 1. set handlers for open gallery events
+    container.addEventListener('click', openHandler);
+    // 2. set handlers for close events
+    btnClose.addEventListener('click', closeHandler);
+    // 3. set handlers for controls events
+    // 4. set handlers for keyboard events
+  }
+
+  openHandler(e) {
+    // console.log('openHandler is working');
+    e.preventDefault();
+    let {
+      openHandler,
+      setCurrentPosition,
+      modifires: {open},
+      gallery,
+    } = this;
+
+    // 0. block scroll
+    document.body.style.cssText = 'overflow: hidden';
+    // 1. get index
+    let currentItem;
+    switch (e.target.tagName) {
+      case 'A':
+        currentItem = e.target;
+        break;
+      case 'IMG':
+        currentItem = e.target.parentNode;
+        break;
+    }
+    // 2. set this.currentIndex
+    this.currentIndex = parseInt(currentItem['dataset']['index']);
+
+    // 3. set position wrapItems
+    // setCurrentPosition();
+    // 4. show gallery
+    gallery.classList.add(open);
+  }
+
+  closeHandler(e) {
+    console.log('close is working');
+    let {
+      modifires: {open},
+      gallery,
+    } = this;
+
+    gallery.classList.remove(open);
+    // 0. block scroll
+    document.body.style.cssText = 'overflow: auto';
+  }
+
+  nextHandler(e) {
+    console.log('nextHandler is working');
+  }
+
+  previousHandler(e) {
+    console.log('previousHandler is working');
+  }
+
+  keyboardHandlers(e) {
+    console.log('keyboardHandlers is working');
+  }
+
+  setCurrentPosition() {}
+
   init() {
     this.createItems();
+    this.setHandlers();
   }
 }
 
