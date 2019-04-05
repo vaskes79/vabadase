993fdb0a72c600de94df26ec8475f8fbcf685d9c
Vasily Guzov <vasily.guzov@gmail.com>
2019-04-05 10:51:35 +0300

    #56 remove FOUC effect

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ import Form from './js/Form';
 import Map from './js/Map';
 import './sass/main.scss';
 
-const slider = new Slider();
-const nav = new Nav();
-const form = new Form();
+document.addEventListener('DOMContentLoaded', () => {
+  document.documentElement.classList.remove('no-fouc');
 
-nav.init();
-slider.init();
-form.init();
+  const slider = new Slider();
+  const nav = new Nav();
+  const form = new Form();
 
-if (window.innerWidth >= 900) {
-  const gallery = new Gallery();
-  const map = new Map();
-  gallery.init();
-  map.init();
-}
+  nav.init();
+  slider.init();
+  form.init();
+
+  if (window.innerWidth >= 900) {
+    const gallery = new Gallery();
+    const map = new Map();
+    gallery.init();
+    map.init();
+  }
+});
